fa6c44049cc236c145ec9c23dec35e78249f899f
agustinsarasua <agustinsarasua@gmail.com>
2017-09-28 19:59:43 -0300

    gradient decent finish

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -2,9 +2,11 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 %GRADIENTDESCENT Performs gradient descent to learn theta
 %   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
 %   taking num_iters gradient steps with learning rate alpha
-
+fprintf('Running gradientDecent with theta = %f\n', theta);
 % Initialize some useful values
 m = length(y); % number of training examples
+
+% J_history will contain the cost of each of the iterations. Each iteration will have a diferent theta associated.
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
@@ -17,15 +19,17 @@ for iter = 1:num_iters
     %
     
     % vector x with alL the population of the cities.
+    % x0(i) is always 1 = X(:,1)
+    % x1(i) value of feature 1 for the ith example. X(:,2)
     x = X(:,2);
     
-    %hypothesis theta(1) y tehta(2) Reales, x vector => h vector
+    %h is a vector that contains the values of the hypothesis for each trainig example.
     h = theta(1) + (theta(2)*x);
     
-    theta_zero = theta(1) - alpha * (1/m) * sum(h-y);
-    theta_one  = theta(2) - alpha * (1/m) * sum((h - y) .* x);
+    t0 = theta(1) - alpha * (1/m) * sum(h-y);
+    t1  = theta(2) - alpha * (1/m) * sum((h - y) .* x);
     
-    theta = [theta_zero; theta_one];
+    theta = [t0; t1];
     % ============================================================
 
     % Save the cost J in every iteration    
