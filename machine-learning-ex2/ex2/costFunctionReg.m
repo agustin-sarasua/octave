e191a432f0c58653cb5e9e37b7da0c071b57072d
agustinsarasua <agustinsarasua@gmail.com>
2017-10-07 10:58:09 -0300

    ex3

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,9 +17,16 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+% X is a matrix of 118x28 (118 training examples of 28 features, colulmn0 = 1) 
+% theta is a column vector of 28x1 
+% X * theta = column vector of 118x1 => hypothesis is a column vector of 118x1
+% y is a column vector of 118x1 full of 0s and 1s
+% -y' * log(hypothesis) = 1x118 * 118x1 = 1x1
+% (1 - y)'*log(1-hypothesis) = 1x118 * 118x1 = 1x1
+% (lambda/(2*m)) = Real number
+% theta_reg'*theta_reg = 1x28 * 28x1 = 1x1
 
 
-% X matrix 100x3, theta vector 3x1 => hypothesis vector 100x1
 hypothesis = sigmoid(X * theta);
 % this is because we do not want to regularize theta0 (which is equal to theta(1))
 shift_theta = theta(2:size(theta));
